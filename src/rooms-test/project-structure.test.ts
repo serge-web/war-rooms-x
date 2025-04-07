6eeeef31cd80822fbe1019327f90d786fc1cc93d
Ian Mayo <ian@planetmayo.com>
2025-04-07 17:58:10 +0100

    jest tests

--- src/rooms-test/project-structure.test.ts
+++ src/rooms-test/project-structure.test.ts
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import * as fs from 'fs'
+import * as path from 'path'
 
 describe('Project Structure', () => {
   // Test project structure follows Vite.js-based monolith architecture
