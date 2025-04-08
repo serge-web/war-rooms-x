41ddbcee6248c1f16fcd1680eda12f31297bb27b
Ian Mayo <ian@planetmayo.com>
2025-04-08 15:48:07 +0100

    polish layout

--- src/components/PlayerView/PlayerView.tsx
+++ src/components/PlayerView/PlayerView.tsx
@@ -1,39 +1,58 @@
 import React from 'react'
 import { Layout } from 'antd'
-import './PlayerView.css'
+import { Header, Footer } from 'antd/es/layout/layout'
 
 const { Sider, Content } = Layout
 
 const PlayerView: React.FC = () => {
+
+
+  const gameStateStyle: React.CSSProperties = {
+    height: 120,
+    backgroundColor: '#fff',
+    border: '1px solid orange'
+  };
+  
+  const roomsStyle: React.CSSProperties = {
+    width: '100%',
+    border: '1px solid green'
+  };
+
+  const adminMessagesStyle: React.CSSProperties = {
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    overflow: 'auto',
+    border: '1px solid yellow'
+  };
+
+  const controlPanelStyle: React.CSSProperties = {
+    width: '300px',
+    border: '1px solid green'
+  };
+  
+  const userDetailsStyle: React.CSSProperties = {
+    height: '100px',
+    border: '1px solid blue'
+  };
+  
+  const layoutStyle = {
+    overflow: 'hidden',
+    border: '1px solid brown'
+  };
+  
+
   return (
-    <Layout className="player-view" data-testid="player-view">
-      <Layout>
-        {/* Left panel - RoomsPanel */}
-        <Content>
-          <div className="rooms-panel" data-testid="rooms-panel">
-            <div className="placeholder-content">RoomsPanel</div>
-          </div>
-        </Content>
-        
-        {/* Right panel - ControlPanel */}
-        <Sider width={200} className="control-panel" data-testid="control-panel">
-          {/* GameState component */}
-          <div className="game-state" data-testid="game-state">
-            <div className="placeholder-content">GameState</div>
-          </div>
-
-          {/* AdminMessages component */}
-          <div className="admin-messages" data-testid="admin-messages">
-            <div className="placeholder-content">AdminMessages</div>
-          </div>
-
-          {/* UserDetails component */}
-          <div className="user-details" data-testid="user-details">
-            <div className="placeholder-content">UserDetails</div>
-          </div>
-          
-        </Sider>
-      </Layout>
+    <Layout style={layoutStyle}>
+      <Content style={roomsStyle}>Rooms</Content>
+      <Sider width="25%" style={controlPanelStyle}>
+        <Layout style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+          <Header style={gameStateStyle}>Game State</Header>
+          <Content style={adminMessagesStyle}>Admin Messages</Content>
+          <Footer style={userDetailsStyle}>User Details</Footer>
+        </Layout>
+      </Sider>
     </Layout>
   )
 }
