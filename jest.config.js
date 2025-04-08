1fffc2ba92c85389f6e21091af78199a00bf9656
Ian Mayo <ian@planetmayo.com>
2025-04-08 16:22:29 +0100

    slightly more content

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
