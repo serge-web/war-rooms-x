16cc552b5a8895c9f0a39496efdc01ac3a08dc1f
Ian Mayo <ian@planetmayo.com>
2025-04-10 09:47:03 +0100

    split tabs into two columns by default, enable popout

--- src/components/PlayerView/Rooms/RoomsList/index.tsx
+++ src/components/PlayerView/Rooms/RoomsList/index.tsx
@@ -3,15 +3,28 @@ import * as FlexLayout from 'flexlayout-react'
 import 'flexlayout-react/style/light.css'
 import RoomContent from '../RoomContent'
 import './index.css'
-import { useWargame } from '../../../../contexts/WargameContext'
+import { useRooms } from './useRooms'
 import { RoomType } from '../../../../types/rooms'
 
+const specialRoom = (room: RoomType): boolean => {
+  return room.roomName.startsWith('__')
+}
+
 const RoomsList: React.FC = () => {
-  const { rooms } = useWargame()
+  const { rooms } = useRooms()
   // Create a FlexLayout model for the rooms
   const model = useMemo((): FlexLayout.Model => {
+    const normalRooms = rooms.filter((room: RoomType) => !specialRoom(room))
+    const convertedRooms = normalRooms.map(room => ({
+      type: 'tab',
+      name: room.naturalName,
+      component: 'room',
+      config: room
+    }))
+    const first = convertedRooms[0]
+    const afterFirst = convertedRooms.slice(1)
     const jsonModel: FlexLayout.IJsonModel = {
-      global: {},
+      global: { tabEnableClose: false, tabEnablePopout: true },
       borders: [],
       layout: {
         type: 'row',
@@ -20,15 +33,17 @@ const RoomsList: React.FC = () => {
           {
             type: 'tabset',
             weight: 50,
-            children: rooms.filter((room: RoomType) => !room.roomName.startsWith('__')).map(room => ({
-              type: 'tab',
-              name: room.naturalName,
-              component: 'room',
-              config: room
-            }))
+            children: [first]
           }
         ]
       }
+    }
+    if (afterFirst.length > 0) {
+      jsonModel.layout.children.push({
+        type: 'tabset',
+        weight: 50,
+        children: afterFirst
+      })
     }
     return FlexLayout.Model.fromJson(jsonModel)
   }, [rooms])
