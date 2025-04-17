f0402348c15a2998b046a356095831bfaf20b426
Ian Mayo <ian@planetmayo.com>
2025-04-17 12:05:01 +0100

    introduce dashboard page

--- src/components/AdminView/index.tsx
+++ src/components/AdminView/index.tsx
@@ -9,12 +9,13 @@ import { ListGroup } from './Resources/groups'
 import { ListUser } from './Resources/users'
 import { ListRoom } from './Resources/rooms'
 import './styles.css'
+import { Dashboard } from './dashboard'
 
 export const AdminView: React.FC = () => {
   const {raDataProvider} = useWargame()
   if (!raDataProvider) return null
   return (
-  <Admin dataProvider={raDataProvider} layout={CustomLayout}>
+  <Admin dataProvider={raDataProvider} layout={CustomLayout} dashboard={Dashboard}>
     <Resource name="groups" list={ListGroup} icon={GroupIcon} options={{label: 'Forces'}}  />
     <Resource name="users" list={ListUser} icon={PersonIcon} options={{label: 'Roles'}} />
     <Resource name="chatrooms" list={ListRoom} icon={ChatIcon} options={{label: 'Rooms'}} />
