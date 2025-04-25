cbb4cfa584bc0a372da5a177cd6cc44fc970af12
Ian Mayo <ian@planetmayo.com>
2025-04-25 11:25:57 +0100

    populate player interface from mock data in browser

--- src/components/PlayerView/Rooms/RoomsList/useRooms.ts
+++ src/components/PlayerView/Rooms/RoomsList/useRooms.ts
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
-import { mockRooms } from './mockRooms';
-import { MockRoom, RoomType } from '../../../../types/rooms-d';
+// import { mockRooms } from './mockRooms';
+import { RoomType } from '../../../../types/rooms-d';
 import { useWargame } from '../../../../contexts/WargameContext';
+import { useIndexedDBData } from '../../../../hooks/useIndexedDBData';
+import { RRoom } from '../../../AdminView/raTypes-d';
 
 export const useRooms = () => {
   const [rooms, setRooms] = useState<RoomType[]>([])  
   const { xmppClient } = useWargame()
+  const { data: mockRooms, loading } = useIndexedDBData('chatrooms')
 
   // TODO - also handle details, extract from the room description
 
@@ -13,13 +16,18 @@ export const useRooms = () => {
     if (xmppClient === undefined) {
       // waiting for login
     } else if (xmppClient === null) {
-      // ok, use mock data
-      setRooms(mockRooms.map((room: MockRoom): RoomType => {
-        return {
-          roomName: room.id,
-          naturalName: room.name,
-        }
-      }))
+      if (!loading) {
+        // ok, use mock data
+        const rooms = mockRooms as RRoom[]
+        console.log('mock rooms', rooms)
+        setRooms(rooms.map((room): RoomType => {
+          return {
+            roomName: room.id,
+            naturalName: room.name,
+          }
+        }))
+
+      } 
     } else {
       // TODO: use real data
       if (xmppClient.mucService) {
@@ -37,7 +45,7 @@ export const useRooms = () => {
         fetchRooms()
       }
     }
-  }, [xmppClient]);
+  }, [xmppClient, mockRooms, loading]);
 
   return { rooms };
 }
