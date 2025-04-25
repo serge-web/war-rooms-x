4aa224782c3e3ec053fa58af60da6fd6730ce5de
Ian Mayo <ian@planetmayo.com>
2025-04-25 13:59:39 +0100

    correctly implement next turn for mock data

--- src/components/PlayerView/GameState/useGameState.ts
+++ src/components/PlayerView/GameState/useGameState.ts
@@ -5,7 +5,9 @@ import { incrementState } from './incrementState'
 import { usePubSub } from '../../../hooks/usePubSub'
 import { useIndexedDBData } from '../../../hooks/useIndexedDBData'
 import { RGameState } from '../../AdminView/raTypes-d'
-import { splitGameState } from '../../../helpers/split-game-state'
+import { splitGameState, mergeGameState } from '../../../helpers/split-game-state'
+import localforage from 'localforage'
+import { prefixKey } from '../../../types/constants'
 
 export const useGameState = () => {
   const { xmppClient } = useWargame()
@@ -29,15 +31,45 @@ export const useGameState = () => {
     }
   }, [xmppClient, mockWargame, loading, gameState])
   
+  const effectiveGameProperties = useMemo(() => {
+    if(xmppClient === undefined) {
+      // don't bother
+    } else if (xmppClient === null) {
+      if (!loading && mockWargame) {
+        const state =  mockWargame[0]
+        const { gameProperties } = splitGameState(state)
+        // split the state into two parts
+        return gameProperties
+      }
+    } else {
+      return gameProperties
+    }
+  }, [xmppClient, mockWargame, loading, gameProperties])
+  
+
   const nextTurn = useCallback(async () => {
-    if (effectiveGameState && gameProperties) {
-      const newState = incrementState(effectiveGameState, gameProperties)
-      const res = await updateDocument(newState)
-      if (!res.success) {
-        console.error('Failed to update game state', res)
+    if (effectiveGameState && effectiveGameProperties) {
+      const newState = incrementState(effectiveGameState, effectiveGameProperties)
+      if (xmppClient === undefined) {
+        // do nothing
+      } else if (xmppClient === null) {
+        // update the game state in the ra-data-local-forage
+        // retrieve the first item in the wargames array
+        const wargame = mockWargame?.[0]
+        if (wargame) {
+          const combinedState = mergeGameState(effectiveGameProperties, newState)
+          // save the game object back to indexed db
+          localforage.setItem(`${prefixKey}wargames`, [combinedState])
+        }
+      } else {
+        const res = await updateDocument(newState)
+        if (!res.success) {
+          console.error('Failed to update game state', res)
+        }
+
       }
     }
-  }, [effectiveGameState, gameProperties, updateDocument])
+  }, [effectiveGameState, effectiveGameProperties, updateDocument, mockWargame, xmppClient])
 
   return { gameState: effectiveGameState, nextTurn }
 }
