448d16f15c19ea47f9d186355c4a6c19e2d70700
Ian Mayo <ian@planetmayo.com>
2025-05-02 13:36:20 +0100

    loading configured map

--- src/components/PlayerView/Rooms/MapContent/index.tsx
+++ src/components/PlayerView/Rooms/MapContent/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react'
 import './index.css'
 import { useRoom } from '../useRoom'
-import { RoomType } from '../../../../types/rooms-d'
+import { MapRoomConfig, RoomDetails, RoomType } from '../../../../types/rooms-d'
 import { ConfigProvider } from 'antd'
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
@@ -13,7 +13,23 @@ interface MapProps {
 
 const MapContent: React.FC<MapProps> = ({ room }) => {
   const { theme, messages } = useRoom(room)
+
+  const config: RoomDetails | undefined = useMemo(() => {
+    if (!room.description) 
+      return undefined
+    const data= JSON.parse(room.description)
+    console.log('parsed data', data)
+    return data
+  }, [room])
   
+  const mapConfig: MapRoomConfig | undefined = useMemo(() => {
+    if (!config) 
+      return undefined
+    if (config.specifics?.roomType !== 'map') 
+      return undefined
+    return config.specifics
+  }, [config])
+
   // Default position - can be updated based on your requirements
   const position: [number, number] = [51.505, -0.09]
   
@@ -24,8 +40,6 @@ const MapContent: React.FC<MapProps> = ({ room }) => {
       return undefined
     }
   }, [messages])
-
-  console.log('featureCollection', featureCollection)
 
   if (!featureCollection) {
     return (
@@ -47,10 +61,10 @@ const MapContent: React.FC<MapProps> = ({ room }) => {
         zoom={8} 
         style={{ height: '100%', width: '100%' }}
       >
-        <TileLayer
+        {mapConfig?.backdropUrl && <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+          url={mapConfig?.backdropUrl}
+        />}
         <GeoJSON data={featureCollection} />
 
       </MapContainer>
