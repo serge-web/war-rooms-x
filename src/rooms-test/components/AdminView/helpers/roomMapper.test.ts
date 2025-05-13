e86178d0f739558ddeaffdf64e08cc7c71732e8b
Ian Mayo <ian@planetmayo.com>
2025-05-13 13:30:31 +0100

    fix tests

--- src/rooms-test/components/AdminView/helpers/roomMapper.test.ts
+++ src/rooms-test/components/AdminView/helpers/roomMapper.test.ts
@@ -115,7 +115,6 @@ describe('roomMapper', () => {
         description: undefined,
         members: ['user1@ubuntu-linux-2404', 'user2@ubuntu-linux-2404'],
         memberGroups: ['force1', 'force2'],
-        broadcastPresenceRoles: ['owner', 'admin', 'member'],
         persistent: true,
         publicRoom: undefined
       })
